d3c34d3e7b3aa2917355e96fe9b4a90181a6d61a
Snegovoy Alex <snegovoy.aleksandr@gmail.com>
2018-06-13 20:18:36 +0300

    Static properties and methods,constructor/destructor

--- Person.php
+++ Person.php
@@ -1,15 +1,22 @@
 <?php
     class Person
     {
+        public static $counter=0;
         private $firstName ='unnamed';
         private $lastName;
+        public function __construct($firstName,$lastName)
+        {
+            $this->firstName = $firstName;
+            $this->lastName = $lastName;
+            self::$counter++;
+        }
 
         public function getFirstName()
         {
             return $this->firstName;
         }
 
-        public function getLastname()
+        public function getLastName()
         {
             return $this->lastName;
         }
@@ -25,8 +32,20 @@
             $this->lastName = $lastName;
         }
 
+
+        public static function getData()
+        {
+
+        }
+
+        public function __destruct()
+        {
+            echo 'Person objects was created '.self::$counter.PHP_EOL;
+        }
+
     }
-    $person = new Person();
+    $person = new Person('Vasya','Petrov');
     $person->setFirstName('Vasya');
     $person->setLastName('Petrov');
+    $person2 = new Person('Nastya','Rezova');
 ?>
